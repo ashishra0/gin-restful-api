1a9b21df80fa01642596622460ef6011ef51ec10
Ashish Rao <ashishrao2598@gmail.com>
2019-12-27 11:03:27 +0530

    initial commit

---
+++ platform/article/article.go
@@ -0,0 +1,29 @@
+package article
+
+// Article struct
+type Article struct {
+	Title string `json:"title"`
+	Body  string `json:"body"`
+	Count int    `json:"int"`
+}
+
+// Repo contains all the articles created
+type Repo struct {
+	Articles []Article
+}
+
+// New is a pointer to the Repo Struct
+func New() *Repo {
+	return &Repo{}
+}
+
+// Add will append new articles to the Repo
+func (r *Repo) Add(article Article) {
+	article.Count = len(article.Body)
+	r.Articles = append(r.Articles, article)
+}
+
+// GetAll will return all the articles in the Repo
+func (r *Repo) GetAll() []Article {
+	return r.Articles
+}
