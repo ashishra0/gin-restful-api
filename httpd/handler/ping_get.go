24c753d6ceca0eea7be408409b48b795cda3bff4
Ashish Rao <ashishrao2598@gmail.com>
2019-12-27 11:02:58 +0530

    add export comment

--- httpd/handler/ping_get.go
+++ httpd/handler/ping_get.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// PingGet endpoint handler function
 func PingGet() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.JSON(http.StatusOK, map[string]string{
